ae76be3bdea7cc4c53da17c117c2ea6289976a46
xjhxcc <2351889342@qq.com>
2019-07-22 20:26:06 +0800

    Feat: create three component

---
+++ src/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import Body from "./components/Body.vue"
+import Footer from "./components/Footer.vue"
+export default {
+  name: 'App',
+  component:{
+    Header,
+    Body,
+    Footer
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
